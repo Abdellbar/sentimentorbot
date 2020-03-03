89e7b897d2daf74faa4fe5017db5c64105e23f85
abdellbar <abdellbar@gmail.com>
2020-03-03 10:16:38 +0100

    make it better

--- sparkapi.py
+++ sparkapi.py
@@ -55,13 +55,14 @@ class Sparkapi:
     """
     url = "https://api.ciscospark.com/v1/memberships"
 
-    payload = "{\n  \"roomId\": \"Y2lzY29zcGFyazovL3VzL1JPT00vNGQ0Yzg2YjAtNWQyNy0xMWVhLWE5OGQtMWY1MWU3NzdkOGRm\",\n  \"personEmail\": \"abdellbar@gmail.com\"\n}"
+    payload = {'roomId': 'Y2lzY29zcGFyazovL3VzL1JPT00vNGQ0Yzg2YjAtNWQyNy0xMWVhLWE5OGQtMWY1MWU3NzdkOGRm',
+               'personEmail': 'abdellbar@gmail.com'}
     headers = {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ZmZkZTJjZGItMTg4NS00MmIxLTk0N2MtZWM0YTUxMjE3ZTU4N2Q0ZjlkODgtZTI4_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'
     }
 
-    response = requests.request("POST", url, headers=headers, data = payload)
+    response = requests.request("POST", url, headers=headers, data = json.dumps(payload))
 
     print(response.text.encode('utf8'))    
     return response
