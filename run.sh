02af86a2dcb6b8e580fb0ba7985734cb187b933c
abdellbar <abdellbar@gmail.com>
2020-03-03 14:18:23 +0100

    first commit

--- run.sh
+++
@@ -1,6 +0,0 @@
-export FLASK_APP=server.py
-export FLASK_DEBUG=1
-export GOOGLE_APPLICATION_CREDENTIALS='/Users/agabdelb/vision bot/secret-brushlands-95547/gservice.json'
-export Webexteams_token='Bearer ZmZkZTJjZGItMTg4NS00MmIxLTk0N2MtZWM0YTUxMjE3ZTU4N2Q0ZjlkODgtZTI4_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'
-flask run --host=0.0.0.0 --port=8080
-#lsof -i
