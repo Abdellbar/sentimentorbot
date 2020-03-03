5b3403453ed32eff47ec6f594c5030d8c2bc0e81
abdellbar <abdellbar@gmail.com>
2020-03-03 14:56:37 +0100

    make it better

---
+++ redeploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+heroku create visionbotai1 #create a new app 
+git init
+heroku git:remote -a visionbotai1
+git add .
+git commit -am "make it better"
+git push heroku master
+heroku config:set GOOGLE_APPLICATION_CREDENTIALS='/app/gservice.json'
+heroku config:set shared_room_id='Y2lzY29zcGFyazovL3VzL1JPT00vNGQ0Yzg2YjAtNWQyNy0xMWVhLWE5OGQtMWY1MWU3NzdkOGRm'
+heroku config:set WEBEX_TEAMS_ACCESS_TOKEN='ZmZkZTJjZGItMTg4NS00MmIxLTk0N2MtZWM0YTUxMjE3ZTU4N2Q0ZjlkODgtZTI4_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'
+heroku ps:scale web=1
+heroku logs -t # display logs
